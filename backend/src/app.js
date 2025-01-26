adf892c2e9a166c6317b6b76280c889d6a553e52
Emilly Paiva <emillypaiva3260@gmail.com>
2025-01-26 17:07:21 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
-const app = express();
+const cors = require('cors');
 
 const router = require('./router');
 
+const app = express();
+
 app.use(express.json());
+app.use(cors());
 app.use(router);
+
+
 
 module.exports = app;
 
