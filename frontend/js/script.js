7697995d8c21ecdae52a58eda333afa8094f98d0
Emilly Paiva <emillypaiva3260@gmail.com>
2025-01-27 22:05:30 -0300

    front finalizado

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,8 @@
 const tbody = document.querySelector('tbody')
 
+const addForm = document.querySelector('.add-form');
 
+const inputTask = document.querySelector('.input-task');
 
 const fetchTasks = async () => {
     const response = await fetch('http://localhost:3333/tasks');
@@ -8,32 +10,156 @@ const fetchTasks = async () => {
     return tasks;
 };
 
+const addTask = async (event)  => {
+    event.preventDefault();
 
 
-const createElement = (tag, innerText = "") => {
+    const task = { title: inputTask.value}
+    
+
+    await fetch('http://localhost:3333/tasks', {
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(task),
+         
+
+    });
+
+    loadTasks();
+    inputTask.value = '';
+    
+}
+
+
+const deleteTask = async (id) => {
+    await fetch(`http://localhost:3333/tasks/${id}`,{
+        method: 'delete',
+    })
+
+    loadTasks();
+}
+
+
+const updateTask =  async (task) => {
+    const {id, title, created_at, status} = task;
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'put',
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({title, status}),
+    })
+
+    loadTasks();
+}
+
+
+const formatDate = (dateUTC) => {
+    const options = { dateStyle: 'long', timeStyle: 'short'}
+    const date = new Date(dateUTC).toLocaleString('pt-br',options);
+    return date;
+}
+
+
+
+
+const createElement = (tag, innerText = "", innerHTML ="") => {
     const element = document.createElement(tag);
-    element.innerText = innerText;
+
+
+    if(innerText){
+        element.innerText = innerText;
+    }
+
+    if(innerHTML){
+        element.innerHTML = innerHTML;
+    }
+    
     return element;
 
 }
 
-const task = {
-    id: 1,
-    title : 'Inscreva-se no canal madual do dev',
-    created_at: '00 de janeiro de 2023 00:12',
-    status: 'pendente'
 
+const createSelect = (value) => {
+     const options = `
+     <option value="pendente">Pendente</option>
+     <option value="em andamento">Em andamento</option>
+     <option value="concluída">Concluída</option>
+     `;
+     const select = createElement('select', '', options);
+
+     select.value = value;
+    return select;
 }
+
+
 
 const createRow = (task) => {
     const {id, title, created_at, status} = task;
     const tr = createElement('tr');
     const tdTitle = createElement('td',title);
+    const tdCreatedAt = createElement('td', formatDate(created_at));
+    const tdStatus = createElement('td');
+    const tdActions= createElement('td');
+
+    const select = createSelect(status);
+    select.addEventListener('change', ({target}) => updateTask({id, title, created_at, status: target.value }));
+
+    const editbutton = createElement('button', '', '<i class="bi bi-pen-fill"></i>');
+    const deletebutton = createElement('button', '', '<i class="bi bi-trash-fill"></i>');
+
+    const editForm = createElement('form');
+    const editInput = createElement('input');
+
+    editInput.value = title;
+    editForm.appendChild(editInput);
+
+    editForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        updateTask({id, title: editInput.value,status});
+
+    })
+
+    editbutton.addEventListener('click', () => {
+        tdTitle.innerText = '';
+        tdTitle.appendChild(editForm);
+    } )
+
+
+    editbutton.classList.add('btn-action');
+    deletebutton.classList.add('btn-action');
+
+    deletebutton.addEventListener('click', () =>deleteTask(id));
+
+    tdStatus.appendChild(select);
+    tdActions.appendChild(editbutton);
+    tdActions.appendChild(deletebutton);
 
     tr.appendChild(tdTitle);
-    tbody.appendChild(tr);
+    tr.appendChild(tdCreatedAt);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdActions);
+
+
+return tr;
+   
 }
 
 
+    const loadTasks = async () => {
+        const tasks = await fetchTasks();
 
-createRow(task);
+        tbody.innerHTML = '';
+        tasks.forEach((task) => {
+            const tr = createRow(task);
+            tbody.appendChild(tr);
+
+        });
+    }
+
+
+    addForm.addEventListener('submit', addTask)
+    loadTasks();
+ 
+
+
+
